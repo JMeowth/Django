0ad543d8b94adb659177adcc73f4bbf4c95ca155
jise51103 <a0909334304@gmail.com>
2022-10-21 07:39:37 -0700

    Define model.py and initiate the admin interface. Define the view.py and ready to use html template

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+	title=models.CharField(max_length=200) #show article title
+	slug=models.CharField(max_length=200) #show article URL
+	body=models.TextField() #show article content
+	pub_date=models.DateTimeField(default=timezone.now)
+	#Make the article display by the pub_date time
+	class Meta:
+		ordering=('-pub_date',)
+
+	def __str__(self):
+		return self.title
